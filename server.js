6b1df45c20955c26f7baff77a0497d9b04ceefe8
Manoj Tambake <manojtambake1122@gmail.com>
2024-07-15 16:26:59 +0530

    live-map project

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.resolve("./public")));
+
+io.on("connection", function (socket) {
+  console.log("User connected", socket.id);
+
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+    console.log("User disconnected", socket.id);
+  });
+});
+
+app.get("/", function (req, res) {
+  res.sendFile("/public/index.html");
+});
+
+server.listen(3000, console.log("App is listening on port 3000"));
